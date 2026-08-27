e17ce8717c764be90cab4cd7ede8d0efb6e498e9
Thomas <77514919+TB516@users.noreply.github.com>
2026-08-27 19:17:11 -0400

    Bump gtkx version

--- gtkx.config.ts
+++ gtkx.config.ts
@@ -4,4 +4,7 @@ export default defineConfig({
   libraries: ["Gtk-4.0", "Adw-1", "Wp-0.5"],
   girPath: ["/app/share/gir-1.0", "/usr/share/gir-1.0"],
   applicationId: "io.github.TB516.mixamp",
+  future: {
+    v2ResourceImports: true,
+  },
 });
